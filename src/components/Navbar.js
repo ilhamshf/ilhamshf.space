4d91cae3d8aa8e7c8843d629b45106eadafb027a
ilhamshf <shevarajacoc@gmail.com>
2021-08-26 20:23:40 +0700

    fix footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumyajit.tech/"
+                href="https://ilhamshf.my.id/blog"
                 target="_blank"
                 rel="noreferrer"
               >
